2beb69229a2300864f825c2bb0739841627c53a0
Goshan57 <485262323@mail.ru>
2025-06-17 20:08:16 +0300

    Created configuration file

--- config.py
+++ config.py
@@ -1,2 +1,23 @@
-MQ_HOST = "0.0.0.0"
-MQ_PORT = 5672
+import pika
+
+
+RMQ_HOST = "0.0.0.0"
+RMQ_PORT = 5672
+
+RMQ_USER = "guest"
+RMQ_PASSWORD = "guest"
+
+connection_params = pika.ConnectionParameters(
+    host=RMQ_HOST,
+    port=RMQ_PORT,
+    credentials=pika.PlainCredentials(
+        username=RMQ_USER,
+        password=RMQ_PASSWORD,
+    ),
+)
+
+
+def get_connection() -> pika.BlockingConnection:
+    return pika.BlockingConnection(
+        parameters=connection_params,
+    )
